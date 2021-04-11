5dfcff3f4f75e68920e4858a06d782c0a6dad8c8
carlosmsilva755 <carlosmsilva755@gmail.com>
2021-04-11 12:15:47 -0300

    feat: initializing project

---
+++ src/components/Photo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles.css'
+
+const Image = () =>{
+    return(
+        <div id='img' >
+        </div>
+    )
+}
+export default Image
---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Image from '../../components/Photo/index'
+import Form from '../../components/Form/index'
+import './styles.css'
+
+const Login = () =>{
+    return(
+        <div id="login">
+			<Image/>
+            <Form/>
+        </div>
+    )
+}
+
+export default Login
